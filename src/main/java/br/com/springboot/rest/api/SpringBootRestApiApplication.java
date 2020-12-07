7ecb6bd65a5125ab03c84356cc7988dfe4a0808c
victorreis <v.crisostomo.reis@gmail.com>
2020-12-07 15:54:02 -0300

    Add checkstyle and formatter and refactor code

---
+++ src/main/java/br/com/springboot/rest/api/SpringBootRestApiApplication.java
@@ -0,0 +1,21 @@
+package br.com.springboot.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class SpringBootRestApiApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApiApplication.class, args);
+    }
+
+}
